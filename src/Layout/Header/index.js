84fbf56d2733e2272049679d7b5e6818eb97915f
Ignacio Palomo <ignacio.palomo@wallbox.com>
2023-01-22 21:21:02 +0100

    fix navigator error

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,25 +1,34 @@
-import './module.header.css'
+import "./module.header.css";
 
-const NavBar = (menu-1, menu-2, menu-3, menu-4) => {
-    return(
+const NavBar = () => {
+  return (
     <>
-    <div className='container-header-NavBar'>
-
-        <div className='container-NavBar'>
-            <span>LOGO</span>
-            <ul className='main-menu'>
-                <li>Mis Articulos</li>
-                <li>Bandeja de Entrada</li>
-                <li>Lista de deseos</li>
-                <li>Vender</li>
-            </ul>
-            <div className="login">
-                <input type='submit' name='button' value='Login' className='user-login'/>
-                <input type='submit' name='button' value='Registrarse' className='user-register'/>
-            </div>
+      <div className="container-header-NavBar">
+        <div className="container-NavBar">
+          <span>LOGO</span>
+          <ul className="main-menu">
+            <li>Mis Articulos</li>
+            <li>Bandeja de Entrada</li>
+            <li>Lista de deseos</li>
+            <li>Vender</li>
+          </ul>
+          <div className="login">
+            <input
+              type="submit"
+              name="button"
+              value="Login"
+              className="user-login"
+            />
+            <input
+              type="submit"
+              name="button"
+              value="Registrarse"
+              className="user-register"
+            />
+          </div>
         </div>
-    </div>
+      </div>
     </>
-    )
-}
-export default NavBar
+  );
+};
+export default NavBar;
