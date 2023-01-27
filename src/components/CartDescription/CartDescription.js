31d7234a55cf6060128f9ced69b6818d555c6eda
Ebed R <ebedleonr@gmail.com>
2023-01-27 20:09:49 +0100

    save work

---
+++ src/components/CartDescription/CartDescription.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './module.cartDescription.css'
+
+import React, { Component } from "react";
+
+class CartDescription  extends Component {
+    render() {
+        return (
+            <h1></h1>
+        );
+    }
+}
+
+export default CartDescription;
