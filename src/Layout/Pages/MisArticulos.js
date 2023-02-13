cf5cab3f75da4cfa1f3be4a22ab218c6a3434566
rortegaez <raulor83@gmail.com>
2023-02-13 11:22:14 +0100

    create component wish-list

--- src/Layout/Pages/MisArticulos.js
+++ src/Layout/Pages/MisArticulos.js
@@ -1,12 +1,38 @@
-import styles from '../Pages/MisArticulos.module.css'
-
+import { useState, useEffect } from "react";
+import styles from "../Pages/MisArticulos.module.css";
+import axios from "axios";
+import CardAllData from "../../components/CardAllData";
 
 export const MisArticulos = () => {
-    return (
-        <>
-        <div className={styles.containerMisArticulos}>
-            <h2>Page Mis Articulos</h2>
-        </div>
-        </>
-    )
-}
+  const [items, setItems] = useState([]);
+
+	//pendiente hacer lógica coger id usuario de localstorage
+
+  async function fetchAllItemsByUser() {
+    const { data } = await axios.get(
+      "http://localhost:8043/clasificados/user/63dd6339c8dd060e29fc46e9/items"
+    );
+
+    return data;
+  }
+
+  useEffect(() => {
+    async function fetchItemsUser() {
+      const { data } = await fetchAllItemsByUser();
+      setItems(data);
+    }
+    fetchItemsUser();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.containerMisArticulos}>
+        <h2>Page Mis Articulos</h2>
+        {console.log(items)}
+        {items.map((item) => (
+          <CardAllData key={item.id} props={item} />
+        ))}
+      </div>
+    </>
+  );
+};
