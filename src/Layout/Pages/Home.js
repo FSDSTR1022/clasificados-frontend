04a041d92cbd3692b6cf694ea8b1af6dbf953b49
Ebed R <ebedleonr@gmail.com>
2023-03-05 16:21:24 +0100

    ccs pagina inicio

--- src/Layout/Pages/Home.js
+++ src/Layout/Pages/Home.js
@@ -1,14 +1,14 @@
 import styles from "../Pages/Home.module.css";
 
 import MainPage from "../MainPage";
-import { ChakraProvider } from "@chakra-ui/react";
+
 
 export const Home = () => {
   return (
     <div className={styles.containerHome}>
-      <ChakraProvider >
+      
         <MainPage/>
-      </ChakraProvider>
-    </div>
+      
+    </div>     
   );
 };
--- src/components/CardItemList/index.js
+++ src/components/CardItemList/index.js
@@ -1,64 +1,40 @@
 import React from "react";
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Image,
-  Stack,
-  Heading,
-  Button,
-} from "@chakra-ui/react";
 import styles from "./carditemlist.module.css";
-
+import axios from "axios"
 const CardItemList = (constructor) => {
-  return (
-    <section className={styles.web}>
-      <div className={styles.articulos}>
-        <div id="carts">
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <Image
-              className={styles.img}
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "200px" }}
-              src={constructor.props.main_image}
-              alt={constructor.props.title}
-            />
+	async function setwishlist(){
+		await axios.post(`${process.env.REACT_APP_LOCALHOST}user/${constructor.props.owner_id}/wish-list/item/${constructor.props.id}`)
+	}
+	return (
+		<section className={styles.web}>
+			<div className={styles.articulos}>
+				<div className={styles.descripcion}>
+					<img className={styles.img} src={constructor.props.main_image} alt={constructor.props.title}></img>
+					<div className={styles.carbody}>
+						<h1 className={styles.head}>
+							{constructor.props.title}
+						</h1>
+						<div className={styles.text}>
+							<div className={styles.textInfo}></div>
+							<div className={styles.precios}>
+								<div className={styles.precio}>
+									<div>Precio: {constructor.props.price}€</div>
+								</div>
+							</div>
+						</div>
+						<footer className={styles.footerCart}>
+							<button className={styles.button} onSubmit={setwishlist}>
+								Añadir a lista deseos
+							</button>
+						</footer>
+					</div>
 
-            <Stack className={styles.descripcion}>
-              <CardBody className={styles.carbody}>
-                <Heading size="md" className={styles.head}>
-                  {constructor.props.title}
-                </Heading>
-                <div className={styles.text}>
-                  <div className={styles.textInfo}></div>
-                  <div className={styles.precios}>
-                    <ul className={styles.precio}>
-                      <li>Precio:</li>
-                      <li>{constructor.props.price}€</li>
-                    </ul>
-                  </div>
-                </div>
-              </CardBody>
+				</div>
 
-              <CardFooter className={styles.footerCart}>
-                <Button
-                  variant="solid"
-                  colorScheme="orange"
-                  className={styles.button}
-                >
-                  Añadir a lista deseos
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-        </div>
-      </div>
-    </section>
-  );
+			</div>
+
+		</section>
+	);
 };
 
 export default CardItemList;
