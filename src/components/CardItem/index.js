7528a4b003ea4ead6917f8d610385f963ab82add
rortegaez <raulor83@gmail.com>
2023-03-05 17:24:04 +0100

    fix2

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -49,7 +49,7 @@ const Carditem = () => {
         <p>{data?.status}</p>
         <p>{data?.reduced_price || "-"}</p>
         <img src={data?.main_image} alt="" />
-        <IMG></IMG>
+        <IMG></IMG><Carditem
         {carImages.map((img, index) => (
           <img key={index} src={img} alt="" />
         ))}
