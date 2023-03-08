8cca14376cb20f8464a48c3e8c1a712a21b0790e
Ebed R <ebedleonr@gmail.com>
2023-03-08 21:30:37 +0100

    css card item

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -1,41 +1,70 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
-import styles from "./module.carditem.css";
+import styles from "./carditem.module.css";
 
-const Carditem = ({ open, onClose, id }) => {
-  const [data, setData] = useState([]);
+const Carditem = () => {
+  const [data, setData] = useState({});
+  const { id } = useParams();
+  console.log(id, "id de url");
 
   async function fetchItem() {
-    const item = await axios.get(
-      `${process.env.REACT_APP_LOCALHOST}/clasificados/item/${id}`
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_LOCALHOST}item/${id}`
     );
-    return item;
+
+    return data;
   }
 
   useEffect(() => {
     async function findItem() {
       const Item = await fetchItem();
-      setData(Item);
+
+      setData({ ...Item });
     }
     findItem();
   }, []);
 
-  if (!open) return null;
+  let carImages = [];
+  console.log("images", data?.images);
+  if (data?.images !== undefined) {
+    data.images.forEach((element) => {
+      console.log("imassssssss", element);
+      carImages = [...carImages, element];
+    });
+  }
+
+  console.log("prueba", carImages);
 
   return (
-    <div className="mainContainer">
-      <h1 className="prueba">
-        <p>{data.data?.title}</p>
-        <p>{data.data?.description}</p>
-        <p>{data.data?.city}</p>
-        <p>{data.data?.country}</p>
-        <p>{data.data?.price}</p>
-        <p>{data.data?.status}</p>
-        <p>{data.data?.reduced_price}</p>
-      </h1>
-      <p onClick={onClose}>X</p>
+    <div className={styles.cardWrapper}>
+      <div className={styles.card}>
+        <div className={styles.productimgs}>
+          <div className={styles.showcase}>
+            <img src={data?.main_image} alt="" className={styles.mainimage} />
+            <div>
+
+              {carImages.map((img, index) => (
+                <img key={index} src={img} alt="" className={styles.img} />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.info}>
+          <p className={styles.title}>{data?.title}</p>
+          <label>Description</label>
+          <p className={styles.description}>{data?.description}</p>
+          <p className={styles.location}><span>Location -</span> {data?.location?.city}</p>
+          <p className={styles.country}> <span>Country - </span> {data?.location?.country}</p>
+          <p className={styles.price}> <span>Price - </span> {data?.price} €</p>
+          <p className={styles.status}><span>Status -</span> {data?.status}</p>
+          <p className={styles.reduced_price}><span>Reduced price </span> {data?.reduced_price || "-"}</p>
+        </div>
+        <button></button>
+      </div>
     </div>
   );
 };
