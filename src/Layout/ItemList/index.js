bb964e9c255e1386c29031c70cb23169426054de
Nacho Ruiz <nachoruizpalomo@gmail.com>
2023-03-08 23:26:50 +0100

    add price filter and paginator

--- src/Layout/ItemList/index.js
+++ src/Layout/ItemList/index.js
@@ -1,20 +1,35 @@
 import React, { useContext } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import CardItemList from "../../components/CardItemList";
 import { FiltersContext } from "../MainPage/context/filters-context";
-
 import styles from "./itemlist.module.css";
+import { promptError } from "../../shared/promptMessages";
 
-const ItemList = () => {
+const ItemList = ({ onTotalCountChange }) => {
   const { filters } = useContext(FiltersContext);
 
   const [itemsList, setItemList] = useState([]);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  async function fetchItemList() {
-    let url = `http://localhost:8043/clasificados/items?page=${filters.page}&page_size=${filters.pageSize}`;
-    console.log(filters);
+  function areFiltersCorrect() {
+    if (filters.minPrice && filters.maxPrice) {
+      if (!isPriceRangeCorrect()) {
+        promptError("precio mínimo no puede ser superior al precio máximo");
+
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function isPriceRangeCorrect() {
+    return parseInt(filters.maxPrice) >= parseInt(filters.minPrice);
+  }
+
+  function createUrl() {
+    let url = `${process.env.REACT_APP_LOCALHOST}/clasificados/items?page=${filters.page}&page_size=${filters.pageSize}`;
 
     if (filters.search) {
       url += `&search_words=${filters.search}`;
@@ -24,17 +39,44 @@ const ItemList = () => {
       url += `&type=${filters.type}`;
     }
 
+    if (filters.minPrice) {
+      url += `&min_price=${filters.minPrice}`;
+    }
+
+    if (filters.maxPrice) {
+      url += `&max_price=${filters.maxPrice}`;
+    }
+
+    return url;
+  }
+
+  function updateTotalCount(totalCount) {
+    onTotalCountChange(totalCount);
+  }
+
+  async function fetchItemList() {
+    const url = createUrl();
+
     const { data } = await axios.get(url);
+
+    updateTotalCount(data.meta.totalCount);
 
     return data;
   }
 
-  useEffect(() => {
-    async function retrieveItemList() {
-      const { data } = await fetchItemList();
-      setItemList(data);
-    }
+  async function retrieveItemList() {
+    if (areFiltersCorrect()) {
+      try {
+        const { data } = await fetchItemList();
 
+        setItemList(data);
+      } catch (error) {
+        promptError(error.response.data.error.message);
+      }
+    }
+  }
+
+  useEffect(() => {
     retrieveItemList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filters]);
@@ -44,6 +86,7 @@ const ItemList = () => {
       {itemsList.map((item) => (
         <CardItemList key={item.id} props={item} />
       ))}
+      <ToastContainer />
     </div>
   );
 };
--- src/Layout/MainPage/index.js
+++ src/Layout/MainPage/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./mainpage.module.css";
 import { FiltersProvider } from "./context/filters-context.js";
-import { useState } from 'react'
+import { useState } from "react";
 
 //Componentes importados
 import Footer from "../Footer/index";
 import ItemTypesList from "../ItemTypesList/index";
 import ItemList from "../ItemList/index";
 import Search from "../../components/Search";
+import { Paginator } from "../../components/Paginator";
 
 const MainPage = () => {
+  const [totalCount, setTotalCount] = useState(0);
+  console.log(totalCount);
 
   return (
     <>
@@ -19,8 +22,9 @@ const MainPage = () => {
             <Search className={styles.search} />
             <div className={styles.mainp}>
               <ItemTypesList />
-              <ItemList />
+              <ItemList onTotalCountChange={setTotalCount} />
             </div>
+            <Paginator totalCount={totalCount} />
           </div>
         </FiltersProvider>
 
