f7a1191218d8b11e2a2aaa250666f53c0e15a4d7
claudio-ms <cmunoz.river@gmail.com>
2023-03-09 20:23:22 +0100

    update MyProducts

--- src/Layout/Pages/MisArticulos.js
+++ src/Layout/Pages/MisArticulos.js
@@ -12,56 +12,39 @@ export const MisArticulos = () => {
   const [items, setItems] = useState([]);
 
   async function fetchAllItemsByUser() {
-
     const {data} = await axios.get('http://localhost:8043/clasificados/user/63dd6339c8dd060e29fc46e9/items');
 
-    console.log(data);
     return data;
   }
 
   useEffect(() => {
     async function fetchItemsUser() {
       const {data} = await fetchAllItemsByUser();
-      // console.log(data);
+      console.log(data);
       setItems(data);
     }
     fetchItemsUser();
   }, []);
 
-  function showDetailsCard() {
-    console.log('He presionado la foto');
-  }
-  console.log(items);
   return (
-      <div className="m-auto w-3/4 pt-14 pb-[380px]">
+      <div className="m-auto w-1/2 pt-16">
         <p className="text-4xl text-gray-500 font-semibold">Mis Articulos</p>
 
-        <div className='pt-8 pb-[380px] justify-items-center grid grid-cols-1 gap-4'>
-          {items.map((item) => (
-            <CardAllData onClick={showDetailsCard}
-              key={item.id}
-              image={item.main_image}
-              title={item.title}
-              type={item.type}
+        <div className='mx-8 pt-8 justify-items-center grid grid-cols-1 gap-4'>
 
-              price={item.price}
-              status={item.status}
-            />
-          ))}
+          {items.map((item, index) => {
+            return (
+              <CardAllData
+                key={item.id}
+                image={item.main_image}
+                images={item.images} 
+                title={item.title}
+                description={item.description}
+                price={item.price}
+                status={item.status}
+              /> )}
+          )}
       </div>
       </div>
 );
 };
-
-/* <div className="absolute bg-sky-200 rounded-md top-20 right-6 w-[700px] h-[800px]"></div>
-  <div className='relative bg-white'>
-    <h2>lkhjsdfjkhjkhsdf</h2>
-
-    title={item.title}
-    price={item.price}
-  </div> */
-
-// type={item.type}
-// low_price={item.reduced_price}
-// description={item.description}
-// status={item.status}
--- src/components/CardAllData/index.js
+++ src/components/CardAllData/index.js
@@ -1,23 +1,34 @@
 import React from 'react'
 
-const CardAllData = ({ image, title, type, price, status }) => {
-	const showDetailsCard = () => {
-		console.log('He presionado la Card');
-	}
+const CardAllData = ({ main_image, images, title, description, price,reduced_price, status }) => {
+
 	return (
 		
-		<div onClick={ showDetailsCard } className='bg-lime-200 w-32 h-28 rounded-t-md cursor-pointer pb-[180px]'> 
+	<div className='bg-slate-500 w-full h-[350px] rounded-t-lg cursor-pointer'> 
+		<div className='flex rounded-t-lg bg-lime-700'>
+			<img className='bg-red-400 w-[250px] h-[250px] object-fit bg-contain rounded-tl-lg align-middle content-center justify-center' src={main_image} alt='without Card' />
 
-		<img className='w-32 h-28 object-fit bg-contain rounded-t-md' src={image} alt='card Items' />
+			<div className='bg-gray-100 w-3/4 h-30 align-text-top rounded-tr-lg bg-gradient-to-l from-indigo-300 to-blue-100'>
+				<p className='text-md text-gray-700 font-300 first-letter:uppercase pt-4'>{title}</p>
+				<p className='text-sm text-gray-600 my-1 mx-8'>{description}</p>
 
-		<div className='bg-gray-300 w-32 h-30 align-text-top rounded-b-md'>
-			<p className='text-xs text-gray-600 font-100 first-letter:uppercase py-2'>{title}</p>
-			<p className='text-sm text-gray-500'>{type}</p>
-			<p className='text-sm text-cyan-300 font-semibold'>Valor: {price} €</p>
-			<p className='my-2 text-xs text-gray-100 font-light'>Estado: {status}</p>
+				<div className='flex justify-around'>
+					<p className='text-sm text-blue-500 mt-2 font-normal'>Valor: {price} €</p>
+					<p className='text-sm text-red-500 mt-2 font-normal'>Oferta: {reduced_price} €</p>
+				</div>
+
+				<p className='flex my-2 text-xs font-normal ml-16 text-gray-600'>Estado: {status}</p>
+
+			</div>
+
+
 		</div>
+
+		<img className='bg-red-500 w-[100px] h-[100px] object-fit' src={images} alt='cual es esta?' />
 
 	</div>
 	)
 }
 export default CardAllData
+
+/* <button className='border-solid border-spacing-2 border-slate-400 text-xs'>Cambiar Precio</button> */
